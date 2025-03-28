5b41f0b716a891d10c5dd20f3bb6b5af6b077142
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2025-03-28 11:22:06 -0300

    xz

---
+++ perguntas.js
@@ -0,0 +1,80 @@
+criarCartao(
+    'História',
+    'Pergunta: Quem foi o primeiro presidente dos Estados Unidos?',
+    'Resposta: George Washington')
+    criarCartao(
+    'Geografia',
+    'Pergunta: Qual é a capital da França? ',
+    'Resposta: Paris'),
+    criarCartao(
+    'Ciência',
+    'Pergunta: Qual é a fórmula química da água?',
+    'Resposta: H2O')
+    criarCartao(
+    'Literatura',
+    'Pergunta: Quem escreveu "Dom Quixote"?',
+    'Resposta: Miguel de Cervantes')
+    criarCartao(
+    'Matemática',
+    'Pergunta: Qual é o valor de pi aproximadamente?',
+    'Resposta: 3,14')
+     criarCartao(
+    'Cinema',
+    'Pergunta: Quais são os três filmes da trilogia "O Senhor dos Anéis"?',
+    'Resposta: A Sociedade do Anel, As Duas Torres, O Retorno do Rei')
+    criarCartao(
+    'Música',
+    'Pergunta: Quem é conhecido como o "Rei do Pop?',
+    'Resposta: Michael Jackson')
+    criarCartao(
+    'Tecnologia',
+    'Pergunta: O que é um computador?',
+    'Resposta: Um dispositivo eletrônico que processa dados.')
+    criarCartao(
+    'Esportes',
+    'Pergunta: Qual país venceu a Copa do Mundo de 2014?',
+    'Resposta: Alemanha')
+    criarCartao(
+    'Biologia',
+    'Pergunta: Qual é a função dos pulmões?',
+    'Resposta: Trocar oxigênio e dióxido de carbono.')
+    criarCartao(
+    'Artes',
+    'Pergunta: Quem pintou a Mona Lisa?',
+    'Resposta: Leonardo da Vinci')
+    criarCartao(
+    'Física',
+    'Pergunta: O que é a gravidade?',
+    'Resposta: A força que atrai os objetos para o centro da Terra.')
+    criarCartao(
+    'Filosofia',
+    'Pergunta: Quem é conhecido como o pai da filosofia ocidental?',
+    'Resposta: Sócrates')
+    criarCartao(
+    'Mitologia',
+    'Pergunta: Quem é o deus do trovão na mitologia nórdica?',
+    'Resposta: Thor')
+    criarCartao(
+    'Culinária',
+    'Pergunta: Qual é o ingrediente principal da paella?',
+    'Resposta: Arroz')
+    criarCartao(
+    'Economia',
+    'Pergunta: O que é inflação?',
+    'Resposta: O aumento geral e contínuo dos preços.')
+    criarCartao(
+    'Medicina',
+    'Pergunta: Qual é a função do coração no corpo humano?',
+    'Resposta: Bombear sangue para todo o corpo.').
+    criarCartao(
+    'Astronomia',
+    'Pergunta: Qual é o planeta mais próximo do Sol?',
+    'Resposta: Mercúrio')
+    criarCartao(
+    'Psicologia',
+    'Pergunta: O que é a psicologia?',
+    'Resposta: O estudo do comportamento e dos processos mentais.')
+    criarCartao(
+    'Linguística',
+    'Pergunta: O que é um idioma?',
+    'Resposta: Um sistema de comunicação usado por uma comunidade')
